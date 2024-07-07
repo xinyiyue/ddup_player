2034a91e723455b135ccbf061934da97c64bc791
yuexinyi <yuexinyi@xiaomi.com>
2024-07-07 21:00:57 +0800

    fix log print wrong when multi-thread print log
    Signed-off-by: yuexinyi <yuexinyi@xiaomi.com>

--- log/ddup_log.h
+++ log/ddup_log.h
@@ -3,26 +3,28 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include "player/util.h"
 
 // 定义日志等级
 typedef enum LogLevel_ { ERROR, WARNING, INFO, DEBUG } LogLevel;
 
 static LogLevel level_ = INFO;
 
+static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
 static const char *get_level_string(LogLevel level) {
   const char *ret;
   switch (level) {
     case ERROR:
-      ret = "ERROR";
+      ret = "E";
       break;
     case INFO:
-      ret = "INFO";
+      ret = "I";
       break;
     case WARNING:
-      ret = "WARNING";
+      ret = "W";
       break;
     case DEBUG:
-      ret = "DEBUG";
+      ret = "D";
       break;
     default:
       break;
@@ -32,10 +34,11 @@ static const char *get_level_string(LogLevel level) {
 
 static void log(LogLevel level, const char *tag, const char *format, ...) {
   if (level > level_) return;
+  AutoLock lock(&log_mutex);
   const char *levelStr = get_level_string(level);
   va_list args;
   va_start(args, format);
-  fprintf(stdout, "[%s]%s: ", levelStr, tag);
+  fprintf(stdout, "[%s][%s]:", levelStr, tag);
   vfprintf(stdout, format, args);
   fprintf(stdout, "\n");
   va_end(args);
